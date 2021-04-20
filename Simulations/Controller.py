3da12917ad40fb3b65c420172d98a5002c795e89
Alex Stephens <astephens7000@yahoo.com>
2021-04-20 13:47:56 -0400

    optimized controller sim

--- Simulations/Controller.py
+++ Simulations/Controller.py
@@ -2,16 +2,22 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import msvcrt
+
 ################################################################################
 WHEELBASE = 10 # in
 LOOK_DIST = 12 # in
 ANGLE_RES = 100
 TOLERABLE_DIST = 10 # in
 ################################################################################
+
+overall_start = time.time()
+
 pose = np.array([
-    -8*12,
+    -7*12,
     5*12,
-    -np.pi/2,
+    np.pi,
     0
 ], dtype="float64").reshape(4,1)
 
@@ -20,11 +26,11 @@ lin_spd = 0
 # waypoint bounds: -144 to 144 in
 # these would come from master commands
 way_x = np.array([
-    -8*12, 6*12, 4*12, 10*12
+    -8*12, 6*12, 4*12, 10*12, 0, 0
 ], dtype="float64")
 way_x = way_x.reshape(way_x.shape[0], 1)
 way_y = np.array([
-    4*12, -2*12, -8*12, -5*12
+    4*12, -2*12, -8*12, -5*12, 0, 8*12
 ], dtype="float64")
 way_y = way_y.reshape(way_y.shape[0], 1)
 way_pts = np.hstack((way_x, way_y))
@@ -34,43 +40,52 @@ tracker = 0
 act_path_x = np.array([])
 act_path_y = np.array([])
 
-#tracking_x = np.array([])
-#tracking_y = np.array([])
+tracking_x = np.array([])
+tracking_y = np.array([])
 itr = 0
 
 dist_i = 0
 
 while 1:
     # generate semicircle
-    angle_start = pose[2] - np.pi/2
-    angle_end   = pose[2] + np.pi/2
+    # angle_start = pose[2] - np.pi
+    # angle_end   = pose[2] + np.pi
 
-    angles = np.linspace(angle_start, angle_end, ANGLE_RES)
+    # angles = np.linspace(angle_start, angle_end, ANGLE_RES)
 
-    x_circ = LOOK_DIST * np.cos(angles) + pose[0]
-    y_circ = LOOK_DIST * np.sin(angles) + pose[1]
+    # x_circ = LOOK_DIST * np.cos(angles) + pose[0]
+    # y_circ = LOOK_DIST * np.sin(angles) + pose[1]
 
-    circ = np.hstack((x_circ, y_circ))
+    # circ = np.hstack((x_circ, y_circ))
 
     # find tracking point on circle compared to current tracking way_pt
-    pt_i = 0
-    min_dist = 1000000
-    for cnt, c_pt in enumerate(circ):
-        dist = np.linalg.norm(way_pts[tracker] - c_pt)
-        if dist < min_dist:
-            pt_i = cnt
-            min_dist = dist
-    #tracking_x = np.append(tracking_x, circ[pt_i,0])
-    #tracking_y = np.append(tracking_y, circ[pt_i,1])
-    dist_i += dist
-    lin_spd = 0.1 * min_dist + 0.003 * dist_i
-    
-    if lin_spd > 11:
-        lin_spd = 11
-    print(lin_spd)
+    # pt_i = 0
+    # min_dist = 1000000
+    # for cnt, c_pt in enumerate(circ):
+    #     dist = np.linalg.norm(way_pts[tracker] - c_pt)
+    #     if dist < min_dist:
+    #         pt_i = cnt
+    #         min_dist = dist
+    # tracking_x = np.append(tracking_x, circ[pt_i,0])
+    # tracking_y = np.append(tracking_y, circ[pt_i,1])
 
-    alpha = angles[pt_i] - pose[2]
-    e_ld = LOOK_DIST * np.sin(alpha)
+    
+    vec1 = np.array([np.cos(pose[2][0]), np.sin(pose[2][0])])
+    intermed_vec = np.array([pose[0][0], pose[1][0]])
+    vec2 = way_pts[tracker] - intermed_vec
+
+    dist_to_goal = np.linalg.norm(pose[0:2].reshape(1,2) - way_pts[tracker])
+
+    ang2 = np.arctan2(vec2[1], vec2[0]) - np.arctan2(vec1[1], vec1[0])
+
+
+    dist_i += dist_to_goal
+    lin_spd = 0.1 * dist_to_goal + 0.003 * dist_i
+    
+    if lin_spd > 20.8:
+        lin_spd = 20.8
+
+    e_ld = LOOK_DIST * np.sin(ang2)
 
     steering_angle = np.arctan2(2 * WHEELBASE * e_ld, LOOK_DIST**2)
 
@@ -92,13 +107,14 @@ while 1:
         speeds[2] * 0.1 + pose[2],
         steering_angle
     ], dtype="float64").reshape(4,1)
+    #print([pose[0][0], pose[1][0]])
 
     # determine if tracking way_pt is close enough to change current way_pt
     if tracker == way_pts.shape[0] - 1:
         tol_dist = 2.5
     else:
         tol_dist = TOLERABLE_DIST
-    dist_to_goal = np.linalg.norm(way_pts[tracker] - pose[0:2].reshape(1,2))
+    
     if dist_to_goal < tol_dist and tracker < way_pts.shape[0]:
         tracker = tracker + 1
 
@@ -110,6 +126,13 @@ while 1:
         break
 
     itr += 1
+
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        if key == b'q':
+            break
+
+print(time.time() - overall_start)
 
 plt.plot(act_path_x, act_path_y, 'r*')
 #plt.plot(tracking_x, tracking_y, 'y*')
